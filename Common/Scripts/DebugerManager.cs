936e2866ac6e3cf7269954ff99eb17f2fa116eee
渊づ祭 <popboyhsf@gmail.com>
2019-11-18 12:12:34 +0800

    修正debug模块方式

--- Common/Scripts/DebugerManager.cs
+++ Common/Scripts/DebugerManager.cs
@@ -541,7 +541,7 @@ public class DebugerManager : MonoBehaviour
                     var j = item.gameObject.GetComponent<IDebuger>();
                     if (j != null)
                     {
-                        DebugerBoolList.Add(new DebugerStruct("无敌", item));
+                        DebugerBoolList.Add(new DebugerStruct(j.AllowName, item));
                     }
                 }
             }
@@ -711,4 +711,5 @@ public struct DebugerStruct
 public interface IDebuger
 {
     bool AllowDebug { get; set; }
+    string AllowName { get; }
 }
